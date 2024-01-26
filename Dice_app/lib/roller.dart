5f3796877b2a43f42d8e5cf1acf80a2674d40743
Azhar Hayat <115901212+Azharhayat271@users.noreply.github.com>
2024-01-26 08:49:32 +0500

    animations added

--- Dice_app/lib/roller.dart
+++ Dice_app/lib/roller.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Roller extends StatefulWidget {
-  @override
   const Roller({Key? key}) : super(key: key);
 
   @override
@@ -10,31 +12,61 @@ class Roller extends StatefulWidget {
 }
 
 class _RollerState extends State<Roller> {
-  int diceValue = 1;
+  late int diceValue;
+  late int newDiceValue;
+  late bool _rolling;
 
-  void rollDice() {
+  @override
+  void initState() {
+    super.initState();
+    diceValue = 1;
+    _rolling = false;
+  }
+
+  Future<void> rollDice() async {
     setState(() {
-      diceValue = 1 + (DateTime.now().millisecondsSinceEpoch % 6);
+      _rolling = true;
+    });
+    await Future.delayed(
+        const Duration(milliseconds: 500)); // Simulate rolling time
+
+    setState(() {
+      newDiceValue = 1 + Random().nextInt(6);
+      diceValue = newDiceValue;
+      _rolling = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 41, 180, 249),
+      backgroundColor: const Color.fromARGB(255, 232, 94, 253),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-              onPressed: () {
-                rollDice();
-              },
-              child: Image.asset(
-                "assets/images/dice-$diceValue.png",
-                width: 200,
-                height: 200,
-              ),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: _rolling
+                  ? SizedBox(
+                      key: ValueKey<int>(diceValue),
+                      child: AnimatedFlip(
+                        key: ValueKey<int>(diceValue),
+                        child: Image.asset(
+                          "assets/images/dice-$diceValue.png",
+                          width: 200,
+                          height: 200,
+                        ),
+                      ),
+                    )
+                  : SizedBox(
+                      key: ValueKey<int>(diceValue),
+                      child: Image.asset(
+                        "assets/images/dice-$diceValue.png",
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
@@ -46,6 +78,50 @@ class _RollerState extends State<Roller> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class AnimatedFlip extends StatefulWidget {
+  final Widget child;
+
+  const AnimatedFlip({Key? key, required this.child}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AnimatedFlipState createState() => _AnimatedFlipState();
+}
+
+class _AnimatedFlipState extends State<AnimatedFlip>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: _animation,
+      child: widget.child,
     );
   }
 }
